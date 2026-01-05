5151bfd97600bdb7450f02ed4f30d5028741225f
Traxmaxx <320272+Traxmaxx@users.noreply.github.com>
2026-01-05 12:46:44 +0100

    remove redundant SPLIT_KEYBOARD setting

--- keyboards/bastardkb/charybdis/4x6/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/rules.mk
@@ -5,8 +5,6 @@ AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
 
-SPLIT_KEYBOARD = yes
-
 POINTING_DEVICE_DRIVER = pmw3360
 
 SERIAL_DRIVER = vendor
